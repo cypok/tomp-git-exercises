bb6edc304a7852f9fb4466f024fb44137c6042eb
IvannZ <zhukov.ivan05@yandex.ru>
2023-10-20 23:47:41 +0700

    Fixed sort.py

--- sort.py
+++ sort.py
@@ -1,12 +1,11 @@
 arr = [40, 4, 20, 10, 30, 6, 10]
 
-
 # insertion sort:
 for i in range(1, len(arr)):
     a_i = arr[i]
     j = i - 1
     while j >= 0:
-        print("comparing {} and {}".format(arr[j], arr_i))
+        print("comparing {} and {}".format(arr[j], a_i))
         if arr[j] > a_i:
             arr[j + 1] = arr[j]
         else:
@@ -15,3 +14,4 @@ for i in range(1, len(arr)):
     arr[j + 1] = a_i
 
 print(arr)
+
